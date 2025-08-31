17a9cd44096b04669e795f749d7ade4a85f99882
Christiankj05 <potetforvw@gmail.com>
2025-09-01 00:09:47 +0200

    Update :Eksempel-1:Log-in-skjerm.php

--- Tema-1-kode/:Eksempel-1:Log-in-skjerm.php
+++ Tema-1-kode/:Eksempel-1:Log-in-skjerm.php
@@ -3,9 +3,9 @@
 /*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
 /*    Programmet skriver ut en "god dag"-melding med personens navn 
 */
-	<h2>Velkommen</h2>
+
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"];  
-	
+  print ("Velkommen! <br />");
   print ("God dag $fornavn $etternavn, ha en god dag! <br />");  
 ?>
