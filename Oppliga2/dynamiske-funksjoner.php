029e4175ef98bbb6fd2f152356c169f5c3246364
Christiankj05 <potetforvw@gmail.com>
2025-10-27 21:55:14 +0100

    Update dynamiske-funksjoner.php

--- Oppliga2/dynamiske-funksjoner.php
+++ Oppliga2/dynamiske-funksjoner.php
@@ -38,7 +38,7 @@ print("<option value='$klassekode'>$klassekode $klassenavn </option>"); /* ny ve
 function listeboksstudent ()
 {
 include("db-tilkobling.php"); /* tilkobling til database-server og valg av database utført */
-$sqlSetning="SELECT * FROM klasse ORDER BY brukernavn;";
+$sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
 $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
 $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
 for ($r=1;$r<=$antallRader;$r++)
