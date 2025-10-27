af61a2b2cf2dbc0a891a6e1c4400da377377e119
Christiankj05 <potetforvw@gmail.com>
2025-10-27 23:48:58 +0100

    Update slett-student.php

--- Oppliga2/slett-student.php
+++ Oppliga2/slett-student.php
@@ -13,3 +13,29 @@
         ?> 
     </select> 
     <br/>
+    <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" />
+</form>
+
+<?php
+if (isset($_POST["slettStudentKnapp"])) {
+    include("db-tilkobling.php");
+    $brukernavn = $_POST["brukernavn"];
+    if (!$brukernavn) {
+        print("Du har ikke valgt en student");
+    } else {
+        $sqlHent = "SELECT fornavn, etternavn, klassekode FROM student WHERE brukernavn='$brukernavn';";
+        $resultat = mysqli_query($db, $sqlHent) or die("Feil ved henting av studentdata");
+        if (mysqli_num_rows($resultat) == 0) {
+            print("Fant ingen student med brukernavn <strong>$brukernavn</strong>.");
+        } else {
+            $rad = mysqli_fetch_array($resultat);
+            $fornavn = $rad["fornavn"];
+            $etternavn = $rad["etternavn"];
+            $klassekode = $rad["klassekode"];
+            $sqlSlett = "DELETE FROM student WHERE brukernavn='$brukernavn';";
+            mysqli_query($db, $sqlSlett) or die("ikke mulig &aring; slette data i databasen");
+            print("Studenten <strong>$fornavn $etternavn</strong> (brukernavn: <strong>$brukernavn</strong>, klasse: <strong>$klassekode</strong>) er nå slettet.");
+        }
+    }
+}
+?>
