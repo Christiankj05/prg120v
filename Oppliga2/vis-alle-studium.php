dd15b3caa63c6c17778da88b640ab20d2c8b95c7
Christiankj05 <potetforvw@gmail.com>
2025-10-27 19:48:40 +0100

    Update vis-alle-studium.php

--- Oppliga2/vis-alle-studium.php
+++ Oppliga2/vis-alle-studium.php
@@ -9,14 +9,14 @@ $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente dat
 $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
 print ("<h3>Registrerte klasser </h3>");
 print ("<table border=1>");
-print ("<tr><th align=left>klassekode</th> <th align=left>klassenavn</th> </tr>");
+print ("<tr><th align=left>klassekode</th> <th align=left>studiumkode</th> <th align=left>klassenavn</th></tr>");
 for ($r=1;$r<=$antallRader;$r++)
 {
 $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
 $klassekode=$rad["klassekode"];
 $studiumkode=$rad["studiumkode"];
 $klassenavn=$rad["klassenavn"];
-print ("<tr> <td> $klassekode </td> <td> $studiumkode </td> ( <td> $klassenavn </td>  ) </tr>");
+print ("<tr> <td> $klassekode </td> <td> $studiumkode </td> <td> $klassenavn </td> </tr>");
 }
 print ("</table>");
 ?>
@@ -38,7 +38,7 @@ $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet
   $studiumnavn=$rad["klassekode"];
 $studiumkode=$rad["studiumkode"];
 $studiumnavn=$rad["klassenavn"];
-print ("<tr> <td> $klassekode </td> <td> $studiumkode </td> ( <td> $klassenavn </td>  ) </tr>");
+print ("<tr> <td> $klassekode </td> <td> $studiumkode </td> <td> $klassenavn </td> </tr>");
 }
 print ("</table>");
 ?>
