fbbd29105c063378668c45c7d3651084b594a2aa
Christiankj05 <potetforvw@gmail.com>
2025-10-27 23:53:40 +0100

    Update slett-studium.php

--- Oppliga2/slett-studium.php
+++ Oppliga2/slett-studium.php
@@ -18,12 +18,13 @@
 
 <?php
 if (isset($_POST["slettKlasseKnapp"])) {
-  include_once("db-tilkobling.php");
+  include("db-tilkobling.php");
   $klassekode = $_POST["klassekode"];
 
   if (!$klassekode) {
     print("Du har glemt å velge en klassekode");
   } else {
+    include("db-tilkobling.php");
     $sqlSetning = "DELETE FROM klasse WHERE klassekode='$klassekode';";
     mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; slette data i databasen");
     print("Denne klassen er nå slettet: $klassekode <br />");
