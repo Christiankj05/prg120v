edc7c625bd0708d1852dc6321ce8dfb7e7e9c623
Christiankj05 <potetforvw@gmail.com>
2025-10-27 23:46:20 +0100

    Update slett-student.php

--- Oppliga2/slett-student.php
+++ Oppliga2/slett-student.php
@@ -1,32 +1,15 @@
 <?php ?>
 <script src="funksjoner.js"></script>
 
-<h3>Slett klasse</h3>
+<h3>Slett student</h3>
 
-<form method="post" action="" id="slettKlasseskjema" name="slettKlasseskjema" onsubmit="return bekreft()">
-  Klasse 
-  <select name="klassekode" id="klassekode">
-    <?php 
-      print("<option value=''>Velg klasse </option>");
-      include("dynamiske-funksjoner.php"); 
-      listeboksKlassekode(); 
-    ?>
-  </select>
-  <br/>
-  <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
-</form>
-
-<?php
-if (isset($_POST["slettKlasseKnapp"])) {
-  include_once("db-tilkobling.php");
-  $klassekode = $_POST["klassekode"];
-
-  if (!$klassekode) {
-    print("Du har glemt å velge en klassekode");
-  } else {
-    $sqlSetning = "DELETE FROM klasse WHERE klassekode='$klassekode';";
-    mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; slette data i databasen");
-    print("Denne klassen er nå slettet: $klassekode <br />");
-  }
-}
-?>
+<form method="post" action="" id="slettStudentkjema" name="slettStudentskjema" onSubmit="return bekreft()">
+    Student 
+    <select name="brukernavn" id="brukernavn">
+        <?php 
+        print("<option value=''>Velg en student å slette</option>");
+        include("dynamiske-funksjoner.php"); 
+        listeboksstudent(); 
+        ?> 
+    </select> 
+    <br/>
