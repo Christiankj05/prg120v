94c8cb11c48921f29a01e5f926e2f8e57455d7d4
Christiankj05 <potetforvw@gmail.com>
2025-10-27 21:35:39 +0100

    Create slett-student.php

---
+++ Oppliga2/slett-student.php
@@ -0,0 +1,31 @@
+<?php 
+?>
+<script src="funksjoner.js"> </script>
+<h3>Slett klasse</h3>
+<form method="post" action="" id="slettKlasseskjema" name="slettKlasseskjema" onSubmit="return
+bekreft()">
+Klasse <select name="klassekode" id="klassekode">
+<?php print("<option value=''>Velg klasse </option>");
+include("dynamiske-funksjoner.php"); listeboksKlassekode(); ?>
+</select> <br/>
+<input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
+</form>
+<?php
+if (isset($_POST ["slettKlasseKnapp"]))
+{
+include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$klassekode=$_POST ["klassekode"];
+if (!$klassekode)
+{
+print ("Det er ikke valgt noe emne");
+}
+else
+{
+include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
+mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+/* SQL-setning sendt til database-serveren */
+print ("Denne klassen er n&aring; slettet: $klassekode <br />");
+}
+}
+?>
